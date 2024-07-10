700595e97e423f7a0006cb47f58e985b17793886
Qassim Osseni <qassimosseni@gmail.com>
2024-07-10 15:29:21 +0200

    ajout du bon calendrier

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_07_09_145651) do
+ActiveRecord::Schema[7.1].define(version: 2024_07_10_123425) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -32,6 +32,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_07_09_145651) do
     t.date "ending_date"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "date_range"
     t.index ["bike_id"], name: "index_bookings_on_bike_id"
     t.index ["user_id"], name: "index_bookings_on_user_id"
   end
